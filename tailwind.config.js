3e3060c02f10e13ec33485caf9d68a43c39b16ab
Molina-Q <quentinmolina1@gmail.com>
2024-08-29 20:17:04 +0200

    chore: Update Tailwind CSS content paths for TypeScript files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: ['class'],
+	content: [
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
+  ],
 	theme: {
 		theme: {
 			container: {
@@ -53,5 +59,5 @@ export default {
 			},
 		},
 	},
-	plugins: [],
+	plugins: [import('tailwindcss-animate')],
 };
