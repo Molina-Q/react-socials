55d5c6009d2a20ee4867a6aa4503452610483e1b
Molina-Q <quentinmolina1@gmail.com>
2024-09-01 12:24:43 +0200

    feat: Implement authentication layout with conditional rendering based on user authentication status

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -1,10 +1,28 @@
-import React from 'react'
+import { Outlet, Navigate } from "react-router-dom"
 
-type Props = {}
+const AuthLayout = () => {
+  const isAuthenticated = false;
 
-const AuthLayout = (props: Props) => {
+
+
   return (
-    <div>AuthLayout</div>
+    <>
+      {isAuthenticated
+        ? (<Navigate to='/' />)
+        : (
+          <>
+            <section className="flex flex-1 justify-center items-center flex-col py-10 px-16">
+              <Outlet />
+            </section>
+
+            <img
+              src="/assets/images/side-img.svg"
+              alt="logo"
+              className="hidden xl:block h-screen w-1/2 object-cover bg-no-repeat"
+            />
+          </>
+        )}
+    </>
   )
 }
 
