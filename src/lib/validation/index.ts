c050b07497be2dfbf89b11b6230db467dad67202
Molina-Q <quentinmolina1@gmail.com>
2024-09-08 20:28:01 +0200

    refactor: Update password validation error message

--- src/lib/validation/index.ts
+++ src/lib/validation/index.ts
@@ -4,5 +4,5 @@ export const SignUpValidation = z.object({
     name: z.string().min(2, { message: 'Too short' }),
     username: z.string().min(2, { message: 'Too short' }),
     email: z.string().email(),
-    password: z.string().min(8, { message: 'Too short' }),  
+    password: z.string().min(8, { message: 'The password must be 8 at least characters long' }),  
 })
