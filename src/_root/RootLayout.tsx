a9262cc66f040713ad553b26bc25898c4df94e96
Molina-Q <quentinmolina1@gmail.com>
2024-10-06 19:31:40 +0200

    refactor: Update RootLayout component to include shared components and layout structure

--- src/_root/RootLayout.tsx
+++ src/_root/RootLayout.tsx
@@ -1,10 +1,24 @@
+
+import BottomBar from '@/components/shared/Bottombar'
+import LeftSidebar from '@/components/shared/LeftSidebar'
+import TopBar from '@/components/shared/TopBar'
 import React from 'react'
+import { Outlet } from 'react-router-dom'
 
 type Props = {}
 
 const RootLayout = (props: Props) => {
   return (
-    <div>RootLayout</div>
+    <div className='w-full md:flex'>
+      <TopBar />
+      <LeftSidebar />
+
+      <section className='flex flex-1 h-full'>
+        <Outlet />
+      </section>
+
+      <BottomBar />
+    </div>
   )
 }
 
