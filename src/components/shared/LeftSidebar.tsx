7b7286dea1f726122d094c407b632c1b79c76397
Molina-Q <quentinmolina1@gmail.com>
2024-10-09 19:05:46 +0200

    feat: Add user profile image to LeftSidebar component

--- src/components/shared/LeftSidebar.tsx
+++ src/components/shared/LeftSidebar.tsx
@@ -1,8 +1,53 @@
-import React from 'react'
+import { useUserContext } from "@/context/AuthContext";
+import { useSignOutAccount } from "@/lib/react-query/querysAndMutations";
+import { useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom"
 
 const LeftSidebar = () => {
+  const { mutate: signOut, isSuccess } = useSignOutAccount()
+  const navigate = useNavigate();
+  const { user } = useUserContext();
+
+  useEffect(() => {
+    if (isSuccess) {
+      navigate(0);
+    }
+  }, [isSuccess])
+
   return (
-    <div>LeftSidebar</div>
+    <div>
+      <nav className="leftsidebar">
+        <div className="flex flex-col gap-11">
+          <Link to="/" className='flex gap-3 items-center'>
+            <img
+              src='/assets/images/logo.svg'
+              alt='logo'
+              width={170}
+              height={36}
+            />
+          </Link>
+
+          <Link 
+            to={`/profile/${user.id}`} 
+            className="flex gap-3 items-center"
+          >
+            <img 
+              src={user.imageUrl || '/assets/icons/profile-placeholder.svg'} 
+              alt="profile"
+              className="size-14 rounded-full"
+            />
+            <div className="flex flex-col">
+              <p className="body-bold">
+                {user.name}
+              </p>
+              <p className="small-regular text-light-3">
+                @{user.username}
+              </p>
+            </div>
+          </Link>
+        </div>
+      </nav>
+    </div>
   )
 }
 
