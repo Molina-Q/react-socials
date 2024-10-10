de85a5477e533cc68bad3c39aaaad9cdd2c30aa4
Molina-Q <quentinmolina1@gmail.com>
2024-10-10 12:00:00 +0200

    Commit for October 10, 2024

--- src/_root/RootLayout.tsx
+++ src/_root/RootLayout.tsx
@@ -1,13 +1,9 @@
-
-import BottomBar from '@/components/shared/Bottombar'
+import BottomBar from '@/components/shared/BottomBar'
 import LeftSidebar from '@/components/shared/LeftSidebar'
 import TopBar from '@/components/shared/TopBar'
-import React from 'react'
 import { Outlet } from 'react-router-dom'
 
-type Props = {}
-
-const RootLayout = (props: Props) => {
+const RootLayout = () => {
   return (
     <div className='w-full md:flex'>
       <TopBar />
