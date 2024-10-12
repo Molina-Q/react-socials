0f78dd3288cccccbdaa498478c2aaca1fe3e34a2
Molina-Q <quentinmolina1@gmail.com>
2024-10-12 19:43:49 +0200

    refactor: Update LeftSidebar component to include sidebarLinks from constants and use NavLink for sidebar navigation

--- src/components/shared/LeftSidebar.tsx
+++ src/components/shared/LeftSidebar.tsx
@@ -1,7 +1,9 @@
+import { sidebarLinks } from "@/constants";
 import { useUserContext } from "@/context/AuthContext";
 import { useSignOutAccount } from "@/lib/react-query/querysAndMutations";
+import { INavLink } from "@/types";
 import { useEffect } from "react";
-import { Link, useNavigate } from "react-router-dom"
+import { Link, NavLink, useNavigate } from "react-router-dom"
 
 const LeftSidebar = () => {
   const { mutate: signOut, isSuccess } = useSignOutAccount()
@@ -27,12 +29,12 @@ const LeftSidebar = () => {
             />
           </Link>
 
-          <Link 
-            to={`/profile/${user.id}`} 
+          <Link
+            to={`/profile/${user.id}`}
             className="flex gap-3 items-center"
           >
-            <img 
-              src={user.imageUrl || '/assets/icons/profile-placeholder.svg'} 
+            <img
+              src={user.imageUrl || '/assets/icons/profile-placeholder.svg'}
               alt="profile"
               className="size-14 rounded-full"
             />
@@ -45,6 +47,26 @@ const LeftSidebar = () => {
               </p>
             </div>
           </Link>
+
+          <ul className="flex flex-col gap-6">
+            {
+              sidebarLinks.map((link: INavLink, index) => (
+                <li key={link.label} className="leftsidebar-link">
+
+                  <NavLink
+                    to={link.route}
+                    className={`flex gap-4 items-center p-4`}
+                  >
+                    <img src={link.imgURL} alt={link.label} className="group-hover:invert-white" />
+                    {link.label}
+
+
+                  </NavLink>
+                </li>
+              ))
+            }
+
+          </ul>
         </div>
       </nav>
     </div>
